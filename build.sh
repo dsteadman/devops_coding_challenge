47903719075a92bf1ed7fdbbbdab32c733816659
Daniel Steadman <steadman.dan@gmail.com>
2024-04-25 17:29:41 -0400

    back to using build.sh for local builds only

--- build.sh
+++ build.sh
@@ -1,24 +1,18 @@
 #!/bin/sh -ex
 
-VERSION=$1
-ENVIRONMENT=$2
-AWS_ACCOUNT=$3
-
-if [ $# -ne 3 ]
-then
-  echo "Usage: $0 <version> <environment> <account_id>"
-  exit 1
-fi
+VERSION=${1:-0.0.0}
+ENVIRONMENT=${2:-staging}
+AWS_ACCOUNT=${3:-1234567890}
 
 IMAGE_ID=ebb-${ENVIRONMENT}-resources-lambda:${VERSION}
 
 cp data/ebbcarbon.yaml dist
 # docker build --no-cache --progress plain --platform linux/amd64 -t $IMAGE_ID .
-docker build --no-cache --platform linux/amd64 -f dist/Dockerfile -t ${IMAGE_ID}
+docker build --platform linux/amd64 -t ${IMAGE_ID} dist
 rm dist/ebbcarbon.yaml
 
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT}.dkr.ecr.us-west-2.amazonaws.com
+# aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT}.dkr.ecr.us-west-2.amazonaws.com
 docker tag ${IMAGE_ID} ${AWS_ACCOUNT}.dkr.ecr.us-west-2.amazonaws.com/${IMAGE_ID}
-docker push ${AWS_ACCOUNT}.dkr.ecr.us-west-2.amazonaws.com/${IMAGE_ID}
+# docker push ${AWS_ACCOUNT}.dkr.ecr.us-west-2.amazonaws.com/${IMAGE_ID}
 
-# docker run --rm --platform linux/amd64 -p 9000:8080 $IMAGE_ID
+docker run --rm --platform linux/amd64 -p 9000:8080 $IMAGE_ID
