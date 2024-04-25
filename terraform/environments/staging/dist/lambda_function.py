df2c9d1cddca2ffb69c322b9a08d221740b379ff
Daniel Steadman <steadman.dan@gmail.com>
2024-04-25 11:49:50 -0400

    TICKET-1234: import bare minimum to lambda

--- terraform/environments/staging/dist/lambda_function.py
+++ terraform/environments/staging/dist/lambda_function.py
@@ -1,15 +1,14 @@
-import sys
-import yaml
-import json
+from yaml import safe_load
+from json import dumps
 
 def handler(event, context):
     with open('ebbcarbon.yaml', 'r') as yaml_file:
-        contents = yaml.safe_load(yaml_file)
+        contents = safe_load(yaml_file)
 
         return {
                 "statusCode": 200,
                 "headers": {
                     "Content-Type": "application/json"
                     },
-                "body": contents
+                "body": dumps(contents["resources"])
                 }
