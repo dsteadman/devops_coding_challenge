561b0aa64a5ff71575d211e7b9a0915d255189a8
Daniel Steadman <steadman.dan@gmail.com>
2024-04-29 13:09:48 -0400

    add log groups

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -1,7 +1,7 @@
 module "staging_resources_lambda" {
   source = "../../modules/resources-lambda/v1"
 
-  image_tag          = "0.0.14"
+  image_tag          = "0.0.17"
   lambda_description = "Return a list of resources via HTTP endpoint"
   environment        = "staging"
   project_name       = "ebb-staging-resources-lambda"
--- terraform/modules/resources-lambda/v1/api_gateway.tf
+++ terraform/modules/resources-lambda/v1/api_gateway.tf
@@ -16,8 +16,8 @@ module "api_gateway" {
   domain_name_certificate_arn = module.primary_acm_certificates.acm_certificate_arn
 
   # Access logs
-  # default_stage_access_log_destination_arn = "arn:aws:logs:eu-west-1:835367859851:log-group:debug-apigateway"
-  # default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
+  default_stage_access_log_destination_arn = module.api_gateway_log_group.cloudwatch_log_group_arn
+  default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
 
   # Routes and integrations
   integrations = {
@@ -35,4 +35,31 @@ module "api_gateway" {
   }
 
   tags = var.tags
+}
+
+module "api_gateway_log_group" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
+  version = "~> 3.0"
+
+  name              = "${var.project_name}-logs"
+  retention_in_days = 7
+}
+
+module "error_alarm" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
+  version = "~> 3.0"
+
+  alarm_name          = "${var.project_name}-error-alarm"
+  alarm_description   = "Bad errors in ${module.api_gateway_log_group.cloudwatch_log_group_name}"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  threshold           = 10
+  period              = 60
+  unit                = "Count"
+
+  namespace   = var.project_name
+  metric_name = "ErrorCount"
+  statistic   = "Maximum"
+
+  # alarm_actions = ["arn:aws:sns:eu-west-1:835367859852:my-sns-queue"]
 }
